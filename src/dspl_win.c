c0821b2efbf8266a66ead46a8b3e31aea83177ff
Dsplib <admin@dsplib.org>
2016-02-20 22:57:45 +0300

    window functions symmetric and periodic supporting

--- src/dspl_win.c
+++ src/dspl_win.c
@@ -24,19 +24,19 @@
 #include <math.h>
 #include "dspl.h"
 
-void dspl_win_bartlett(double *w, int n, int win_type);
-void dspl_win_bartlett_hann(double *w, int n);
-void dspl_win_blackman(double *w, int n);
-void dspl_win_blackman_harris(double *w, int n);
-void dspl_win_blackman_nuttall(double *w, int n);
-void dspl_win_flat_top(double *w, int n);
-void dspl_win_gaussian(double *w, double sigma, int n);
-void dspl_win_hamming(double *w, int n);
-void dspl_win_hann(double *w, int n);
-void dspl_win_lanczos(double *w, int n);
-void dspl_win_nuttall(double *w, int n);
-void dspl_win_rect(double *w, int n);
-void dspl_win_cos(double *w, int n);
+void dspl_win_bartlett			(double *w, int n, int win_type);
+void dspl_win_bartlett_hann		(double *w, int n, int win_type);
+void dspl_win_blackman			(double *w, int n, int win_type);
+void dspl_win_blackman_harris	(double *w, int n, int win_type);
+void dspl_win_blackman_nuttall	(double *w, int n, int win_type);
+void dspl_win_flat_top			(double *w, int n, int win_type);
+void dspl_win_gaussian			(double *w, double sigma, int n, int win_type);
+void dspl_win_hamming			(double *w, int n, int win_type);
+void dspl_win_hann				(double *w, int n, int win_type);
+void dspl_win_lanczos			(double *w, int n, int win_type);
+void dspl_win_nuttall			(double *w, int n, int win_type);
+void dspl_win_rect				(double *w, int n);
+void dspl_win_cos				(double *w, int n, int win_type);
 
 
 
@@ -47,19 +47,19 @@ DSPL_API int dspl_window(double* w, int n, int win_type, double param)
 	if(n<2)
 		return DSPL_ERROR_SIZE;
 	switch(win_type & DSPL_WIN_MASK){
-		case  DSPL_WIN_BARTLETT			:	dspl_win_bartlett(w, n, win_type);			break; 
-		case  DSPL_WIN_BARTLETT_HANN	:	dspl_win_bartlett_hann(w, n);		break; 
-		case  DSPL_WIN_BLACKMAN			:	dspl_win_blackman(w, n);			break; 
-		case  DSPL_WIN_BLACKMAN_HARRIS	:	dspl_win_blackman_harris(w, n);		break; 
-		case  DSPL_WIN_BLACKMAN_NUTTALL	:	dspl_win_blackman_nuttall(w, n);	break; 
-		case  DSPL_WIN_FLAT_TOP			:	dspl_win_flat_top(w, n);			break; 
-		case  DSPL_WIN_GAUSSIAN			:	dspl_win_gaussian(w, param, n);		break; 
-		case  DSPL_WIN_HAMMING			:	dspl_win_hamming(w, n);				break; 
-		case  DSPL_WIN_HANN				:	dspl_win_hann(w, n);				break; 
-		case  DSPL_WIN_LANCZOS			:	dspl_win_lanczos(w, n);				break; 
-		case  DSPL_WIN_NUTTALL			:	dspl_win_nuttall(w, n);				break; 
-		case  DSPL_WIN_RECT				:	dspl_win_rect(w, n);				break; 
-		case  DSPL_WIN_COS				:	dspl_win_cos(w, n);					break;
+		case  DSPL_WIN_BARTLETT			:	dspl_win_bartlett			(w, n, win_type);	break; 
+		case  DSPL_WIN_BARTLETT_HANN	:	dspl_win_bartlett_hann		(w, n, win_type);	break; 
+		case  DSPL_WIN_BLACKMAN			:	dspl_win_blackman			(w, n, win_type);	break; 
+		case  DSPL_WIN_BLACKMAN_HARRIS	:	dspl_win_blackman_harris	(w, n, win_type);	break; 
+		case  DSPL_WIN_BLACKMAN_NUTTALL	:	dspl_win_blackman_nuttall	(w, n, win_type);	break; 
+		case  DSPL_WIN_FLAT_TOP			:	dspl_win_flat_top			(w, n, win_type);	break; 
+		case  DSPL_WIN_GAUSSIAN			:	dspl_win_gaussian			(w, param, n, win_type);		break; 
+		case  DSPL_WIN_HAMMING			:	dspl_win_hamming			(w, n, win_type);	break; 
+		case  DSPL_WIN_HANN				:	dspl_win_hann				(w, n, win_type);	break; 
+		case  DSPL_WIN_LANCZOS			:	dspl_win_lanczos			(w, n, win_type);	break; 
+		case  DSPL_WIN_NUTTALL			:	dspl_win_nuttall			(w, n, win_type);	break; 
+		case  DSPL_WIN_RECT				:	dspl_win_rect				(w, n);				break; 
+		case  DSPL_WIN_COS				:	dspl_win_cos				(w, n, win_type);	break;
 		default 						:	return DSPL_ERROR_WIN_TYPE;			
 	}
 	return DSPL_OK;
@@ -71,10 +71,10 @@ void dspl_win_bartlett(double *w, int n, int win_type)
 {
 	double x; 
 	int i;
-	if((win_type & DSPL_WIN_TYPE_MASK) == DSPL_WIN_SYMMETRIC)
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_SYMMETRIC)
 		x = (double)(n-1);
 
-	if((win_type & DSPL_WIN_TYPE_MASK) == DSPL_WIN_PERIODIC)
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_PERIODIC)
 		x = (double)n;
 
 	for(i = 0; i < n; i++)
@@ -87,64 +87,100 @@ void dspl_win_bartlett(double *w, int n, int win_type)
 
 
 /* Barlett - Hann window */
-void dspl_win_bartlett_hann(double *w, int n)
+void dspl_win_bartlett_hann(double *w, int n, int win_type)
 {
 	double y;
+	double x;
 	int i;
+	
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_SYMMETRIC)
+		x = 1.0/(double)(n-1);
+
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_PERIODIC)
+		x = 1.0/(double)n;
+
+	y = 0.0;
 	for(i = 0; i<n; i++)
 	{
-		y = (double)i /(double)(n-1);
 		w[i] = 0.62 - 0.48*fabs(y-0.5)-0.38*cos(M_2PI*y);
+		y += x;
 	}
 }
 
 
 
 /* Blackman window */
-void dspl_win_blackman(double *w, int n)
+void dspl_win_blackman(double *w, int n, int win_type)
 {
 	double y;
+	double x;
 	int i;
+	
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_SYMMETRIC)
+		x = M_2PI/(double)(n-1);
+
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_PERIODIC)
+		x = M_2PI/(double)n;
+
+	y = 0.0;
 	for(i = 0; i<n; i++)
 	{
-		y = 2*M_PI*(double)i /(double)(n-1);
 		w[i] = 0.42 - 0.5* cos(y)+0.08*cos(2.0*y);
+		y += x;
 	}
 }
 
 
 
 /* Blackman - Harris window */
-void dspl_win_blackman_harris(double *w, int n)
+void dspl_win_blackman_harris(double *w, int n, int win_type)
 {
 	double y;
+	double x;
 	double a0 = 0.35875;
 	double a1 = 0.48829;
 	double a2 = 0.14128;
 	double a3 = 0.01168;
 	int i;
+	
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_SYMMETRIC)
+		x = M_2PI/(double)(n-1);
+
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_PERIODIC)
+		x = M_2PI/(double)n;
+
+	y = 0.0;	
 	for(i = 0; i<n; i++)
 	{
-		y = M_2PI*(double)i /(double)(n-1);
 		w[i] = a0 - a1* cos(y)+a2*cos(2.0*y)-a3*cos(3.0*y);
+		y += x;
 	}
 }
 
 
 
 /* Blackman - Nuttul window */
-void dspl_win_blackman_nuttall(double *w, int n)
+void dspl_win_blackman_nuttall(double *w, int n, int win_type)
 {
 	double y;
+	double x;
 	double a0 = 0.3635819;
 	double a1 = 0.4891775;
 	double a2 = 0.1365995;
 	double a3 = 0.0106411;
 	int i;
+	
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_SYMMETRIC)
+		x = M_2PI/(double)(n-1);
+
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_PERIODIC)
+		x = M_2PI/(double)n;
+
+	y = 0.0;	
 	for(i = 0; i<n; i++)
 	{
-		y = M_2PI*(double)i /(double)(n-1);
 		w[i] = a0 - a1* cos(y)+a2*cos(2.0*y)-a3*cos(3.0*y);
+		y += x;
 	}
 }
 
@@ -152,30 +188,46 @@ void dspl_win_blackman_nuttall(double *w, int n)
 
 
 /* Flat - Top window */
-void dspl_win_flat_top(double *w, int n)
+void dspl_win_flat_top(double *w, int n, int win_type)
 {
 	double y;
+	double x;
 	double a0 = 1.0;
 	double a1 = 1.93;
 	double a2 = 1.29;
 	double a3 = 0.388;
 	double a4 = 0.032;
-	int i;
+	int i;	
+	
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_SYMMETRIC)
+		x = M_2PI/(double)(n-1);
+
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_PERIODIC)
+		x = M_2PI/(double)n;
+
+	y = 0.0;		
 	for(i = 0; i<n; i++)
 	{
-		y = M_2PI*(double)i/(double)(n-1);
 		w[i] = a0 - a1* cos(y)+a2*cos(2.0*y)-a3*cos(3.0*y)+a4*cos(4.0*y);
+		y += x;
 	}
 }
 
 
 
 /* Gaussian window */
-void dspl_win_gaussian(double *w, double alpha, int n)
+void dspl_win_gaussian(double *w, double alpha, int n, int win_type)
 {
 	double x = (double)(n-1)*0.5; 
 	double y;
 	int i;
+	
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_SYMMETRIC)
+		x = (double)(n-1)*0.5;
+
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_PERIODIC)
+		x = (double)(n)*0.5;
+	
 	for(i = 0; i<n; i++)
 	{
 		y = ((double)i - x)/(alpha*x);
@@ -187,55 +239,99 @@ void dspl_win_gaussian(double *w, double alpha, int n)
 
 
 /* Hamming window */
-void dspl_win_hamming(double *w, int n)
+void dspl_win_hamming(double *w, int n, int win_type)
 {
+	double x;
+	double y;
 	int i;
+	
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_SYMMETRIC)
+		x = M_2PI/(double)(n-1);
+
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_PERIODIC)
+		x = M_2PI/(double)n;
+	
+	y = 0.0;
 	for(i = 0; i < n; i++)
-		w[i] = 0.54-0.46*cos(M_2PI*(double)i/(double)(n-1));
+	{
+		w[i] = 0.54-0.46*cos(y);
+		y += x;
+	}
 }
 
 
 
 /* Hann window */
-void dspl_win_hann(double *w, int n)
+void dspl_win_hann(double *w, int n, int win_type)
 {
+	double x;
+	double y;
 	int i;
+	
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_SYMMETRIC)
+		x = M_2PI/(double)(n-1);
+
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_PERIODIC)
+		x = M_2PI/(double)n;
+	
+	y = 0.0;
 	for(i = 0; i < n; i++)
-		w[i] = 0.5*(1-cos(M_2PI*(double)i/(double)(n-1)));
+	{
+		w[i] = 0.5*(1-cos(y));
+		y += x;
+	}
 }
 
 
 
 /* Lanczos window */
-void dspl_win_lanczos(double *w, int n)
+void dspl_win_lanczos(double *w, int n, int win_type)
 {
+	double y;
 	double x; 
 	int i;
+	
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_SYMMETRIC)
+		x = M_2PI/(double)(n-1);
+
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_PERIODIC)
+		x = M_2PI/(double)n;
+	
+	y = 0.0;
 	for(i = 0; i < n; i++)
 	{
-		x = 2.0*(double)i/((double)(n-1))-1;
-		if(x==0.0) 
+		if((y - M_PI)==0.0) 
 			w[i] = 1.0;
 		else 
-			w[i] = sin(M_PI*x)/(M_PI*x);
+			w[i] = sin(y - M_PI)/(y - M_PI);
+		y += x;
 	}
 	
 }
 
 
 /* Nuttal window */
-void dspl_win_nuttall(double *w, int n)
+void dspl_win_nuttall(double *w, int n, int win_type)
 {
 	double y;
+	double x;
 	double a0 = 0.355768;
 	double a1 = 0.487396;
 	double a2 = 0.144232;
 	double a3 = 0.012604;
 	int i;
+	
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_SYMMETRIC)
+		x = M_2PI/(double)(n-1);
+
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_PERIODIC)
+		x = M_2PI/(double)n;
+	
+	y = 0.0;	
 	for(i = 0; i < n; i++)
 	{
-		y = M_2PI*(double)i /(double)(n-1);
 		w[i] = a0 - a1* cos(y)+a2*cos(2.0*y)-a3*cos(3.0*y);
+		y += x;
 	}
 }
 
@@ -252,11 +348,24 @@ void dspl_win_rect(double *w, int n)
 
 
 /* Sine window */
-void dspl_win_cos(double *w, int n)
+void dspl_win_cos(double *w, int n, int win_type)
 {
+	double y;
+	double x;
 	int i;
+	
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_SYMMETRIC)
+		x = M_PI/(double)(n-1);
+
+	if((win_type & DSPL_WIN_SYM_MASK) == DSPL_WIN_PERIODIC)
+		x = M_PI/(double)n;
+	
+	y = 0.0;
 	for(i = 0; i<n; i++)
-		w[i] = sin(M_PI*(double)i/(double)(n-1));
+	{
+		w[i] = sin(y);
+		y += x;
+	}
 }
 
 
