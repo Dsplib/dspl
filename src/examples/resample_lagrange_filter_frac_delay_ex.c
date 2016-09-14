51e70c94c7fa213af810c074d7837fd17e2c548c
Dsplib <bahurin@mail.ru>
2016-09-14 23:54:45 +0300

    Changes to be committed: 	new file:   bin/examples/tex/tmp/resample_lagrange_frac_delay_ex.tex 	modified:   bin/release/dspl64/bin/dspl.dll 	modified:   bin/release/dspl64/src/main.c 	modified:   src/dspl/doxygen/doc_begin_use.dox 	modified:   src/examples/resample_lagrange_filter_frac_delay_ex.c

--- src/examples/resample_lagrange_filter_frac_delay_ex.c
+++ src/examples/resample_lagrange_filter_frac_delay_ex.c
@@ -15,9 +15,19 @@ int main()
 
 	HINSTANCE hDSPL;		/* dspl handle 						*/
 	
-	double s[N] = {1.0, 1.0, 1.5, -0.5, -1.0, -1.5, -1.5, 1.0};
-	double *y = NULL;	
-	int ny;
+	/* input signal vector s = [1 2 2 1 -0.5 -1 -2 -0.5] */
+	double s[N] = {1.0, 2.0, 2.0, 1.0, -0.5, -1.0, -2.0, -0.5};
+	
+	/* 	vector z keeps s and 3 additional zeros 		
+		z = [0 0 1 2 2 1 -0.5 -1 -2 -0.5 0]			
+		zeros are necessary for n-3 to be a nonzero. */ 
+	double z[N+3];	
+	
+	double y[N];	/*output signal delayed on X0 fractional delay 	*/
+	double x;		/* x_k value */
+	double delta;	/* delta_k value */
+	double a[4];	/* Polynomial coeff vector */
+	int k, n;		
 		
     /* Load dspl.dll */
 	hDSPL = dspl_load();
@@ -27,9 +37,43 @@ int main()
 		return 0;
 	}
 
-	/* Allocation memory and fill vectors */
-	dspl_resample_lagrange(s, N, P, Q, 0.25, &y, &ny);
-			
+	/* z = [0 0 1 2 2 1 -0.5 -1 -2 -0.5 0]	*/
+	memset(z, 0, (N+3) * sizeof(double));
+	memcpy(z+2, s, N*sizeof(double));
+	
+	/* table header print */
+	printf("\n\n -----------------------------------------------------");
+	printf("-----------------------------------------------------\n");
+	printf(" | k     xk    n   delta | s(n-3)  s(n-2)  s(n-1)  s(n) |");
+	printf("     a0       a1       a2        a3   |   y(k)   |\n");
+	printf(" -----------------------------------------------------");
+	printf("-----------------------------------------------------\n");
+	
+	for(k = 0; k < N; k++)
+	{
+		x = (double)k * Q/P - X0;
+		n = floor(x)+2;
+		n+=2;
+		delta = - floor(x) - 1.0 + x;
+		
+		/* polynomial coefficients calculation */
+		a[0] = z[n-1];
+		a[3] = (z[n] - z[n-3]) / 6.0 + 0.5 * (z[n-2] - z[n-1]);
+		a[1] = 0.5*(z[n] - z[n-2]) - a[3];
+		a[2] = z[n-1] - z[n] - a[3] - a[1];
+		
+		
+		dspl_polyval(a,3,&delta, 1, y+k);
+		
+		printf(" | %.1d  %6.2f   %.1d  %6.2f |", k, x, n-2, delta);
+		printf(" %6.1f  %6.1f %6.1f %6.1f |", z[n-3], z[n-2], z[n-1], z[n]);
+		printf(" %8.4f  %8.4f %8.4f %8.4f |", a[0], a[1], a[2], a[3]);
+		printf(" %8.4f |\n", y[k]);
+	}
+	
+	printf(" -----------------------------------------------------");
+	printf("-----------------------------------------------------\n\n");	
+	
 	/* clear dspl handle */	
 	FreeLibrary(hDSPL);
 	return 0;
