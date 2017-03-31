95388a715fbeb06b552d1b5cfb4d5739109c5cf5
Dsplib <admin@dsplib.org>
2017-04-01 00:24:14 +0300

    changed dir structure

---
+++ bin/examples/figures/dat2cm.bat
@@ -0,0 +1,67 @@
+dat2cm.exe -i ../dat/dtmf_sym_0_time.txt -o ../dat/cm/dtmf_sym_0_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_1_time.txt -o ../dat/cm/dtmf_sym_1_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_2_time.txt -o ../dat/cm/dtmf_sym_2_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_3_time.txt -o ../dat/cm/dtmf_sym_3_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_4_time.txt -o ../dat/cm/dtmf_sym_4_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_5_time.txt -o ../dat/cm/dtmf_sym_5_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_6_time.txt -o ../dat/cm/dtmf_sym_6_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_7_time.txt -o ../dat/cm/dtmf_sym_7_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_8_time.txt -o ../dat/cm/dtmf_sym_8_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_9_time.txt -o ../dat/cm/dtmf_sym_9_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_a_time.txt -o ../dat/cm/dtmf_sym_a_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_b_time.txt -o ../dat/cm/dtmf_sym_b_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_c_time.txt -o ../dat/cm/dtmf_sym_c_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_d_time.txt -o ../dat/cm/dtmf_sym_d_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_r_time.txt -o ../dat/cm/dtmf_sym_r_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_s_time.txt -o ../dat/cm/dtmf_sym_s_time.cm -ymin -2 -ymax 2 -w 4.5 -h 2
+
+dat2cm.exe -i ../dat/dtmf_sym_0_freq.txt -o ../dat/cm/dtmf_sym_0_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_1_freq.txt -o ../dat/cm/dtmf_sym_1_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_2_freq.txt -o ../dat/cm/dtmf_sym_2_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_3_freq.txt -o ../dat/cm/dtmf_sym_3_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_4_freq.txt -o ../dat/cm/dtmf_sym_4_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_5_freq.txt -o ../dat/cm/dtmf_sym_5_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_6_freq.txt -o ../dat/cm/dtmf_sym_6_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_7_freq.txt -o ../dat/cm/dtmf_sym_7_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_8_freq.txt -o ../dat/cm/dtmf_sym_8_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_9_freq.txt -o ../dat/cm/dtmf_sym_9_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_a_freq.txt -o ../dat/cm/dtmf_sym_a_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_b_freq.txt -o ../dat/cm/dtmf_sym_b_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_c_freq.txt -o ../dat/cm/dtmf_sym_c_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_d_freq.txt -o ../dat/cm/dtmf_sym_d_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_r_freq.txt -o ../dat/cm/dtmf_sym_r_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+dat2cm.exe -i ../dat/dtmf_sym_s_freq.txt -o ../dat/cm/dtmf_sym_s_freq.cm -ymin 0 -ymax 120 -xmin 500 -xmax 1800 -w 4.5 -h 2
+
+
+
+
+dat2cm.exe -i ../dat/resample_lagrange_filter_fd_time_0.0.txt -o ../dat/cm/resample_lagrange_filter_fd_time_0.0.cm  -ymin 0 -ymax 1 -xmin 0 -xmax 3 -w 2.4 -h 2
+dat2cm.exe -i ../dat/resample_lagrange_filter_fd_time_0.1.txt -o ../dat/cm/resample_lagrange_filter_fd_time_0.1.cm  -ymin 0 -ymax 1 -xmin 0 -xmax 3 -w 2.4 -h 2
+dat2cm.exe -i ../dat/resample_lagrange_filter_fd_time_0.2.txt -o ../dat/cm/resample_lagrange_filter_fd_time_0.2.cm  -ymin 0 -ymax 1 -xmin 0 -xmax 3 -w 2.4 -h 2
+dat2cm.exe -i ../dat/resample_lagrange_filter_fd_time_0.3.txt -o ../dat/cm/resample_lagrange_filter_fd_time_0.3.cm  -ymin 0 -ymax 1 -xmin 0 -xmax 3 -w 2.4 -h 2
+dat2cm.exe -i ../dat/resample_lagrange_filter_fd_time_0.4.txt -o ../dat/cm/resample_lagrange_filter_fd_time_0.4.cm  -ymin 0 -ymax 1 -xmin 0 -xmax 3 -w 2.4 -h 2
+dat2cm.exe -i ../dat/resample_lagrange_filter_fd_time_0.5.txt -o ../dat/cm/resample_lagrange_filter_fd_time_0.5.cm  -ymin 0 -ymax 1 -xmin 0 -xmax 3 -w 2.4 -h 2
+dat2cm.exe -i ../dat/resample_lagrange_filter_fd_time_0.6.txt -o ../dat/cm/resample_lagrange_filter_fd_time_0.6.cm  -ymin 0 -ymax 1 -xmin 0 -xmax 3 -w 2.4 -h 2
+dat2cm.exe -i ../dat/resample_lagrange_filter_fd_time_0.7.txt -o ../dat/cm/resample_lagrange_filter_fd_time_0.7.cm  -ymin 0 -ymax 1 -xmin 0 -xmax 3 -w 2.4 -h 2
+dat2cm.exe -i ../dat/resample_lagrange_filter_fd_time_0.8.txt -o ../dat/cm/resample_lagrange_filter_fd_time_0.8.cm  -ymin 0 -ymax 1 -xmin 0 -xmax 3 -w 2.4 -h 2
+dat2cm.exe -i ../dat/resample_lagrange_filter_fd_time_0.9.txt -o ../dat/cm/resample_lagrange_filter_fd_time_0.9.cm  -ymin 0 -ymax 1 -xmin 0 -xmax 3 -w 2.4 -h 2
+
+dat2cm.exe -i ../dat/resample_lagrange_interp_y.txt -o ../dat/cm/resample_lagrange_interp_y.cm  -ymin -2 -ymax 2 -xmin 0 -xmax 7 -w 14 -h 4 -dy -2
+
+dat2cm.exe -i ../dat/resample_lagrange_ex_fs_s.txt -o ../dat/cm/resample_lagrange_ex_fs_s.cm  -ymin -1 -ymax 1 -xmin 0 -xmax 2 -w 16 -h 4 -dy -2
+dat2cm.exe -i ../dat/resample_lagrange_ex_fs_y.txt -o ../dat/cm/resample_lagrange_ex_fs_y.cm  -ymin -1 -ymax 1 -xmin 0 -xmax 2 -w 16 -h 4 -dy -2
+dat2cm.exe -i ../dat/resample_lagrange_ex_fs_c.txt -o ../dat/cm/resample_lagrange_ex_fs_c.cm  -ymin -1 -ymax 1 -xmin 0 -xmax 2 -w 16 -h 4 -dy -2
+
+dat2cm.exe -i ../dat/resample_spline_ex_fs_s.txt -o ../dat/cm/resample_spline_ex_fs_s.cm  -ymin -1 -ymax 1 -xmin 0 -xmax 2 -w 16 -h 4 -dy -2
+dat2cm.exe -i ../dat/resample_spline_ex_fs_y.txt -o ../dat/cm/resample_spline_ex_fs_y.cm  -ymin -1 -ymax 1 -xmin 0 -xmax 2 -w 16 -h 4 -dy -2
+dat2cm.exe -i ../dat/resample_spline_ex_fs_c.txt -o ../dat/cm/resample_spline_ex_fs_c.cm  -ymin -1 -ymax 1 -xmin 0 -xmax 2 -w 16 -h 4 -dy -2
+dat2cm.exe -i ../dat/resample_spline_ex_fs_z.txt -o ../dat/cm/resample_spline_ex_fs_z.cm  -ymin -1 -ymax 1 -xmin 0 -xmax 2 -w 16 -h 4 -dy -2
+dat2cm.exe -i ../dat/resample_spline_ex_fs_el.txt -o ../dat/cm/resample_spline_ex_fs_el.cm  -ymin -0.15 -ymax 0.15 -xmin 0 -xmax 2 -w 16 -h 4 -dy -2
+dat2cm.exe -i ../dat/resample_spline_ex_fs_es.txt -o ../dat/cm/resample_spline_ex_fs_es.cm  -ymin -0.15 -ymax 0.15 -xmin 0 -xmax 2 -w 16 -h 4 -dy -2
+
+
+dat2cm.exe -i ../dat/resample_spline_interp_y.txt -o ../dat/cm/resample_spline_interp_y.cm  -ymin -2 -ymax 2 -xmin 0 -xmax 7 -w 14 -h 4 -dy -2
+
+
+mingw32-make
+
